30b7b0fbf319b368e044004f08f9f2ab5641b4c5
Айрат Ахмадеев <vtb4112062@corp.dev.vtb>
2025-06-19 21:41:44 +0300

    update bot

--- run_forever.py
+++ run_forever.py
@@ -1,17 +1,3 @@
 from bot import main
-import threading
-import time
 
-def run_looped_analysis():
-    while True:
-        print("🔄 Автоматический анализ рынка...")
-        main()
-        time.sleep(300)  # 5 минут
-
-if __name__ == "__main__":
-    # Запускаем автоанализ в отдельном потоке
-    thread = threading.Thread(target=run_looped_analysis)
-    thread.start()
-
-    # Запускаем Telegram-бота
-    main()
+main()
